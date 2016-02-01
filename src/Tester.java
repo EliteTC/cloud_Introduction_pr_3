1358825b556d841e9e77ade2b2cc9a28b69c8c55
EliteTC <deadend1408@gmail.com>
2016-02-01 21:59:02 +0200

    tester file changed

--- src/Tester.java
+++ src/Tester.java
@@ -2,8 +2,8 @@
 public class Tester {
 	public static void main(String [] args){
 		System.out.println("Just for test!!!!!!!!!");
-		int intvar = 23;
-		String what = "whatttt";
+		int intvar = 25;
+		String what = "what";
 		double doublevalue=22.666666;
 	}
 }
