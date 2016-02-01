f7bc874bf26b7e7a1322a7118dd815fea4dfe76e
EliteTC <EliteTC@users.noreply.github.com>
2016-02-01 17:29:57 +0200

    first commit

---
+++ src/Tester.java
@@ -0,0 +1,9 @@
+
+public class Tester {
+	public static void main(String [] args){
+		System.out.println("Just for test!!!!!!!!!");
+		int intvar = 23;
+		String what = "whatttt";
+		double doublevalue=22.666666;
+	}
+}
